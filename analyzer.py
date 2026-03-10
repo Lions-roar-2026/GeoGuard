ad885bd3fc7d1d0443ac7bb8650312329c8814bc
Leo <ono.group2014@gmail.com>
2026-03-10 15:15:32 +0200

    updated to support sync files

--- analyzer.py
+++ analyzer.py
@@ -3,10 +3,10 @@ import geoip2.database
 import os
 
 from geoip2.errors import AddressNotFoundError
-
+from utils.sync import sync_db_file
 from utils.anomaly import detect_anomalies
 
-# classify IP addresses as server/bot
+# classify ip addresses as server/bot
 CLOUD_KEYWORDS = ['amazon', 'google', 'microsoft', 'azure', 'digitalocean',
                   'hetzner', 'ovh', 'linode', 'alibaba', 'oracle', 'host']
 
@@ -27,40 +27,46 @@ def get_asn_org(ip, reader):
 
 
 def run_analysis(input_file, output_file):
-    db_path = 'GeoLite2-ASN.mmdb'
+    # setup configuration for DB files
+    db_configs = [
+        {"source": r'C:\dbmaxmined\GeoLite2-ASN.mmdb', "dest": 'GeoLite2-ASN.mmdb'},
+        {"source": r'C:\dbmaxmined\GeoLite2-Country.mmdb', "dest": 'GeoLite2-Country.mmdb'}
+    ]
+
+    # sync all databases
+    print("Starting database sync...")
+    for db in db_configs:
+        sync_db_file(db['source'], db['dest'])
+
+    # verify files existence
+    for db in db_configs:
+        if not os.path.exists(db['dest']):
+            print(f"error db file {db['dest']} not found!")
+            return
     results = []
+    print(f"starting analysis on {input_file}...")
 
-    if not os.path.exists(db_path):
-        print(f"error: Database file '{db_path}' not found!")
-        return
-
-    print(f"Starting analysis on {input_file}...")
-
-    with geoip2.database.Reader(db_path) as reader, \
+    with geoip2.database.Reader('GeoLite2-ASN.mmdb') as asn_reader, \
+            geoip2.database.Reader('GeoLite2-Country.mmdb') as country_reader, \
             open(input_file, 'r') as f_in, \
             open(output_file, 'w', newline='', encoding='utf-8') as f_out:
 
         writer = csv.writer(f_out)
-        writer.writerow(['IP address', 'Organization', 'Classification'])
+        writer.writerow(['ip address', 'organization', 'classification'])
 
         for line in f_in:
             ip = line.split('#')[0].strip()
             if not ip: continue
 
-            org_name = get_asn_org(ip, reader)
-
-            # check if the organization name contains any cloud/server keywords
+            org_name = get_asn_org(ip, asn_reader)
             is_bot = any(k in org_name.lower() for k in CLOUD_KEYWORDS)
             user_type = "bot/server" if is_bot else "pc/mobile"
 
             results.append({'ip': ip, 'type': user_type})
-
             writer.writerow([ip, org_name, user_type])
-            print(f"processed {ip}: {user_type}")
+            print(f"Processed {ip}: {user_type}")
 
-    print(f"success report saved to: {output_file}")
-
-    print(f"debug: Total results length: {len(results)}")
+    print(f"Success report saved to: {output_file}")
     detect_anomalies(results, threshold=50)
 
 
